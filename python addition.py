c5e98965b84df9ffe587b9ef741bb7a01330af6a
sriharshadv8811 <35754248+sriharshadv8811@users.noreply.github.com>
2018-06-14 13:06:09 +0530

    Add files via upload

---
+++ python addition.py
@@ -0,0 +1,35 @@
+a=1
+b=5
+sum=a+b
+print(sum)
+
+
+a=10
+b=100
+a=b
+print(a)
+
+
+a="cmr"
+b="university"
+print(a+b)
+
+
+a="cmr"
+b="university"
+print("a+b")
+
+
+a=14
+b=9
+sub=a-b
+print(sub)
+
+
+a=24
+b=14
+print("multiplication of a and b")
+print(a*b)
+
+print("division of a and b")
+print(a/b)
